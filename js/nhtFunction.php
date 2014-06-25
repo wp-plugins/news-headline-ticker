e694a8f67243cbce88d02821b6a510eb5c4844e2
csehasib <csehasib@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 22:03:23 +0000

    Fix some issue
    git-svn-id: https://plugins.svn.wordpress.org/news-headline-ticker/trunk@938773 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/nhtFunction.php
+++
@@ -1,5 +0,0 @@
-<?php
-$nhtOptions = array(
-'nhtDiv1' => '<a href="http://www.e2soft.com/" title="Web Design Bangladesh" class="nhtColor" rel="dofollow">Web Design Bangladesh</a>',
-'nhtDiv2' => '<a href="http://www.bdonlinebazar.com/" title="Bangladesh online Market" class="nhtColor" rel="dofollow">Bangladesh online Market</a>');
-?>
