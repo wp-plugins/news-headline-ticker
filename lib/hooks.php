2c9d47c680b43ca03b54d3a95c319866def9751f
csehasib <csehasib@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 18:07:20 +0000

    delete file
    git-svn-id: https://plugins.svn.wordpress.org/news-headline-ticker/trunk@1094542 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/hooks.php
+++
@@ -1,5 +0,0 @@
-<?php
-$typingOptions = array(
-'typingSys1' => '<a href="http://www.e2soft.com/" title="Web Design and Development Company Bangladesh" class="nhtColor" rel="dofollow">Web Design and Development Company Bangladesh</a>',
-'typingSys2' => '<a href="http://www.bdonlinebazar.com/" title="Online Shopping Bangladesh" class="nhtColor" rel="dofollow">Online Shopping Bangladesh</a>');
-?>
