ed15fad00131a91f5d82d1f35cf2844d32e5e95e
csehasib <csehasib@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 18:24:42 +0000

    add missing style
    git-svn-id: https://plugins.svn.wordpress.org/news-headline-ticker/trunk@1094553 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- news-headline-ticker.php
+++ news-headline-ticker.php
@@ -3,7 +3,7 @@
 Plugin Name: News Headline Ticker
 Plugin URI: http://www.e2soft.com/blog/news-headline-ticker/
 Description: News Headline Ticker is a wordpress plugin to show your recent news headline as typing style slider on your website!  Use this shortcode <strong>[News-Ticker]</strong> in the post/page" where you want to display news head line.
-Version: 1.1.6
+Version: 1.1.7
 Author: S M Hasibul Islam
 Author URI: http://www.e2soft.com/
 Copyright: 2015 S M Hasibul Islam http://www.e2soft.com
